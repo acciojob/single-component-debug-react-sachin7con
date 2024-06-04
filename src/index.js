63698245e888d7e013accf2efa06716fa1c7d153
sachin7con <sachin7con@gmail.com>
2024-06-04 11:28:13 +0530

    Update App and Render using DOM

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-
+// Ensure the App component is rendered into the DOM node with id="root"
+ReactDOM.render(<App />, document.getElementById('root'));
